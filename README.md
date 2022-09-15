3624ebe966df9b10ecf776eb2576ab05fc5cf414
Filip Strajnar <filip.strajnar@gmail.com>
2022-09-15 22:24:35 +0200

    initial

---
+++ README.md
@@ -0,0 +1,11 @@
+Reset file:
+
+```
+python change.py reset
+```
+
+Add reverse proxy with HTTPS:
+
+```
+python change.py add "domain.com" "127.0.0.1:9000"
+```
---
+++ conf/change.py
@@ -0,0 +1,97 @@
+#   Copyright 2022 Filip Strajnar
+#
+#   Licensed under the Apache License, Version 2.0 (the "License");
+#   you may not use this file except in compliance with the License.
+#   You may obtain a copy of the License at
+#
+#       http://www.apache.org/licenses/LICENSE-2.0
+#
+#   Unless required by applicable law or agreed to in writing, software
+#   distributed under the License is distributed on an "AS IS" BASIS,
+#   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#   See the License for the specific language governing permissions and
+#   limitations under the License.
+import json
+import sys
+
+def add_reverse_proxy(from_domain: str,to_address: str):
+
+    caddy_config=open("caddy.json","r")
+    entire_config=json.load(caddy_config)
+    
+    # Get certificate for new domain name
+    entire_config["apps"]["tls"]["automation"]["policies"][0]["subjects"].append(from_domain)
+    # Add reverse proxy handler
+    entire_config["apps"]["http"]["servers"]["multi"]["routes"].append({
+                            "match": [
+                                {
+                                    "host": [
+                                        from_domain
+                                    ]
+                                }
+                            ],
+                            "handle": [
+                                {
+                                    "handler": "reverse_proxy",
+                                    "upstreams": [
+                                        {
+                                            "dial": to_address
+                                        }
+                                    ]
+                                }
+                            ]
+                        })
+
+    caddy_config.close()
+    # Write changes made to configuration file
+    caddy_config=open("caddy.json","w")
+    json.dump(entire_config,caddy_config,indent=4)
+    caddy_config.close()
+
+def reset():
+    # Reset to default configuration
+    default_conf='''{
+    "apps": {
+        "tls": {
+            "automation": {
+                "policies": [
+                    {
+                        "subjects": [],
+                        "issuers": [
+                            {
+                                "module": "acme"
+                            }
+                        ],
+                        "renewal_window_ratio": 0.5,
+                        "key_type": "p384"
+                    }
+                ]
+            },
+            "cache": {
+                "capacity": 10000
+            }
+        },
+        "http": {
+            "http_port": 80,
+            "https_port": 443,
+            "servers": {
+                "multi": {
+                    "listen": [
+                        ":443"
+                    ],
+                    "routes": []
+                }
+            }
+        }
+    }
+}'''
+    caddy_config=open("caddy.json","w")
+    caddy_config.write(default_conf)
+    caddy_config.close()
+
+if(sys.argv[1]=="reset"):
+    reset()
+elif(sys.argv[1]=="add"):
+    add_reverse_proxy(sys.argv[2],sys.argv[3])
+else:
+    print("Invalid command.")
